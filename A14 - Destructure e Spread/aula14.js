e1c9a86682a7da2fc78477cf9c690297a31e5d28
darilodsg <139563258+darilodsg@users.noreply.github.com>
2024-04-26 16:51:49 -0300

    A15

---
+++ A14 - Destructure e Spread/aula14.js
@@ -0,0 +1,7 @@
+
+
+const arr = ['Danilo','João','Pedro','Ana','Maria']
+
+const [a,b,...c] = arr
+
+console.log(c[2])
---
+++ A9 - DOM/aula9.js
@@ -0,0 +1,7 @@
+var p = document.getElementsByTagName('p');
+
+p[0].innerHTML = 'Mudei'
+
+for(var i = 0; i < 10; i++){
+    document.write(`Número ${i} » <br>`)
+}
